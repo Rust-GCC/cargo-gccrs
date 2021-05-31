1cbd5a6b3e488420465503c80308505f703b0836
CohenArthur <arthur.cohen@epita.fr>
2021-05-31 12:50:36 +0200

    bin: Wait for cargo-gccrs to exit, print args in non-release mode

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,19 @@ use gccrs::Gccrs;
 fn spawn_as_wrapper() {
     // Skip `cargo` and `gccrs` in the invocation. Since we spawn a new cargo command,
     // `cargo gccrs arg0 arg1` will become `cargo run arg0 arg1`
-    std::process::Command::new("cargo")
+    let mut cargo_gccrs = std::process::Command::new("cargo")
         .env("RUSTC_WRAPPER", "cargo-gccrs")
         .args(std::env::args().skip(2))
         .spawn()
-        .expect("Unable to launch cargo gccrs as RUSTC_WRAPPER");
+        .expect("Unable to launch cargo-gccrs as RUSTC_WRAPPER");
+
+    cargo_gccrs.wait().expect("Subprocess cargo-gccrs didn't complete properly");
 }
 
 fn main() {
+    #[cfg(not(release))]
+    dbg!(std::env::args());
+
     Gccrs::maybe_install().expect("gccrs should be installed");
 
     let first_arg = std::env::args().nth(1);
