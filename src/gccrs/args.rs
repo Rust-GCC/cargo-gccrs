835dcb7feeb90765639a5d56bb5413d6c7cec3ba
CohenArthur <arthur.cohen@epita.fr>
2021-06-12 01:27:35 +0200

    args: Remove format_output_filename() from GccrsArgs's implementation
    Co-authored-by: flip1995 <hello@philkrones.com>

--- src/gccrs/args.rs
+++ src/gccrs/args.rs
@@ -1,6 +1,8 @@
 //! This module interprets arguments given to `rustc` and transforms them into valid
 //! arguments for `gccrs`.
 
+use std::path::PathBuf;
+
 use getopts::{Matches, Options};
 
 use super::{Error, Result};
@@ -31,12 +33,51 @@ impl CrateType {
     }
 }
 
+fn format_output_filename(
+    matches: &Matches,
+    crate_type: CrateType,
+) -> Result<(PathBuf, CrateType)> {
+    // Return an [`Error::InvalidArg`] error if `--crate-name` or `out-dir` weren't
+    // given as arguments at this point of the translation
+    let crate_name = matches.opt_str("crate-name").ok_or(Error::InvalidArg)?;
+    let out_dir = matches.opt_str("out-dir").ok_or(Error::InvalidArg)?;
+    let c_options = matches.opt_strs("C");
+
+    let mut output_file = PathBuf::from(&out_dir);
+
+    // FIXME: Horrendous. We need to create a separate "C options" parser since we'll
+    // probably use more than just `extra-filename`. Issue #6 on Rust-GCC/cargo-gccrs
+    let extra_filename = c_options
+        .iter()
+        .filter_map(|c_opt| {
+            let mut split = c_opt.split('=');
+
+            if let Some("extra-filename") = split.next().as_deref() {
+                split.next()
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<&str>>()[0];
+
+    match crate_type {
+        CrateType::Bin => output_file.push(&format!("{}{}", crate_name, extra_filename)),
+        CrateType::DyLib => output_file.push(&format!("lib{}.so{}", crate_name, extra_filename)),
+        CrateType::StaticLib => output_file.push(&format!("lib{}.a{}", crate_name, extra_filename)),
+        _ => unreachable!(
+            "gccrs cannot handle other crate types than bin, dylib or staticlib at the moment"
+        ),
+    }
+
+    Ok((output_file, crate_type))
+}
+
 /// Structure used to represent arguments passed to `gccrs`. Convert them from `rustc`
 /// arguments using [`GccrsArg::from_rustc_arg`]
 pub struct GccrsArgs {
     source_files: Vec<String>,
     crate_type: CrateType,
-    output_file: String,
+    output_file: PathBuf,
 }
 
 impl GccrsArgs {
@@ -65,43 +106,6 @@ impl GccrsArgs {
         options
     }
 
-    fn format_output_filename(
-        matches: &Matches,
-        crate_type: CrateType,
-    ) -> Result<(String, CrateType)> {
-        // Return an [`Error::InvalidArg`] error if `--crate-name` or `out-dir` weren't
-        // given as arguments at this point of the translation
-        let crate_name = matches.opt_str("crate-name").ok_or(Error::InvalidArg)?;
-        let out_dir = matches.opt_str("out-dir").ok_or(Error::InvalidArg)?;
-
-        // FIXME: Figure out a way to return multiple output filenames. Just handle `bin`
-        // for now
-        let c_options = matches.opt_strs("C");
-
-        // FIXME: This is probably different on Windows, we should use Paths and PathStrs
-        // instead
-        let mut output_file = match crate_type {
-            CrateType::Bin => format!("{}/{}", out_dir, crate_name),
-            CrateType::DyLib => format!("{}/lib{}.so", out_dir, crate_name),
-            CrateType::StaticLib => format!("{}/lib{}.a", out_dir, crate_name),
-            _ => unreachable!(
-                "gccrs cannot handle other crate types than bin, dylib or staticlib at the moment"
-            ),
-        };
-
-        // FIXME: Horrendous. We need to create a separate "C options" parser since we'll
-        // probably use more than just `extra-filename`.
-        c_options.iter().for_each(|c_opt| {
-            let mut split = c_opt.split('=');
-
-            if let Some("extra-filename") = split.next().as_deref() {
-                output_file.push_str(&split.next().unwrap())
-            }
-        });
-
-        Ok((output_file, crate_type))
-    }
-
     /// Get the corresponding `gccrs` argument from a given `rustc` argument
     pub fn from_rustc_args(rustc_args: &[String]) -> Result<Vec<GccrsArgs>> {
         let options = GccrsArgs::generate_parser();
@@ -113,7 +117,7 @@ impl GccrsArgs {
             .opt_strs("crate-type")
             .iter()
             .map(|type_str| CrateType::from_str(&type_str))
-            .map(|crate_type| GccrsArgs::format_output_filename(&matches, crate_type))
+            .map(|crate_type| format_output_filename(&matches, crate_type))
             .map(|result_tuple| {
                 result_tuple.map(|(output_file, crate_type)| GccrsArgs {
                     source_files: matches.free.clone(),
@@ -125,20 +129,23 @@ impl GccrsArgs {
     }
 
     /// Convert a `GccrsArgs` structure into arguments usable to spawn a process
-    pub fn into_args(mut self) -> Vec<String> {
-        let mut args = vec![];
+    pub fn into_args(self) -> Vec<String> {
+        let mut args = self.source_files;
 
-        // Add all the source files to the command line
-        self.source_files
-            .drain(..)
-            .for_each(|source| args.push(source));
+        // FIXME: How does gccrs behave with non-unicode filenames? Is gcc[rs] available
+        // on the OSes that support non-unicode filenames?
+        let output_file = self
+            .output_file
+            .to_str()
+            .expect("Cannot handle non-unicode filenames yet")
+            .to_owned();
 
         match self.crate_type {
-            CrateType::Bin => args.append(&mut vec![String::from("-o"), self.output_file]),
+            CrateType::Bin => args.append(&mut vec![String::from("-o"), output_file]),
             CrateType::DyLib => args.append(&mut vec![
                 String::from("-shared"),
                 String::from("-o"),
-                self.output_file,
+                output_file,
             ]),
             CrateType::StaticLib => unreachable!("Cannot generate static libraries yet"),
             _ => {}
