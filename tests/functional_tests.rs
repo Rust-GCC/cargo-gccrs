6691642a23011dfe958d5f6e4b3f0a44984c6c24
CohenArthur <arthur.cohen@epita.fr>
2021-07-05 00:33:23 +0200

    functional_tests: Add FIXME for invalid test

--- tests/functional_tests.rs
+++ tests/functional_tests.rs
@@ -12,6 +12,8 @@ mod tests {
         Harness::check_folder("static_lib", FileType::Static).unwrap();
         Harness::check_folder("shared_library", FileType::Dyn).unwrap();
 
+        // FIXME: As of right now, this just fails on rustc compilation which is not what
+        // we want to check
         assert!(Harness::check_folder("invalid_code", FileType::Bin).is_err())
     }
 }
